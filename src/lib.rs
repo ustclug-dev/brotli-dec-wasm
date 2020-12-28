d9a3df79b11be535bbf0184f884ca494652b438c
myl7 <myl.ustc@gmail.com>
2020-12-28 08:24:53 +0800

    Update return type
    To release

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,11 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn brotli_dec(buf: Box<[u8]>) -> Result<String, JsValue> {
+pub fn brotli_dec(buf: Box<[u8]>) -> Result<Box<[u8]>, JsValue> {
     let mut out = Vec::<u8>::new();
     match BrotliDecompress(&mut buf.as_ref(), &mut out) {
         Ok(_) => (),
         Err(_) => return Err(JsValue::from_str("brotli dec failed")),
     }
-    match String::from_utf8(out) {
-        Ok(s) => Ok(s),
-        Err(_) => return Err(JsValue::from_str("utf-8 dec failed")),
-    }
+    Ok(out.into_boxed_slice())
 }
