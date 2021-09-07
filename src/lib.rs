3b07db02e2a93b43007b3ab31bc8c1cdf2c8f362
myl7 <myl@myl.moe>
2021-09-07 19:03:56 +0800

    refactor: Format rust code

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,10 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen(js_name = brotliDec)]
 pub fn brotli_dec(buf: Box<[u8]>) -> Result<Box<[u8]>, JsValue> {
-  let mut out = Vec::<u8>::new();
-  match BrotliDecompress(&mut buf.as_ref(), &mut out) {
-    Ok(_) => (),
-    Err(_) => return Err(JsValue::from_str("brotli dec failed")),
-  }
-  Ok(out.into_boxed_slice())
+    let mut out = Vec::<u8>::new();
+    match BrotliDecompress(&mut buf.as_ref(), &mut out) {
+        Ok(_) => (),
+        Err(_) => return Err(JsValue::from_str("brotli dec failed")),
+    }
+    Ok(out.into_boxed_slice())
 }
