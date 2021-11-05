5574fe913c5ac0132145ea77c16f502599f63e59
myl7 <myl@myl.moe>
2021-11-05 15:57:44 +0800

    fix: Fix var ref

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen(js_name = brotliDec)]
 pub fn brotli_dec(input: Box<[u8]>) -> Result<Box<[u8]>, JsValue> {
     let mut output = Vec::new();
-    match BrotliDecompress(&mut input.as_ref(), &mut out) {
+    match BrotliDecompress(&mut input.as_ref(), &mut output) {
         Ok(_) => Ok(output.into_boxed_slice()),
         Err(_) => Err(JsValue::from_str("brotli dec failed")),
     }
