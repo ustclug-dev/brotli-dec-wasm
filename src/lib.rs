36b193e262c9aef4000c07f902531d609193447b
myl7 <myl@myl.moe>
2022-05-04 19:04:52 +0800

    Remove feature stream since the size is not a problem

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 mod err;
-#[cfg(feature = "stream")]
 mod stream;
 mod utils;
 
 use brotli_decompressor::BrotliDecompress;
 pub use err::*;
-#[cfg(feature = "stream")]
 pub use stream::*;
 use wasm_bindgen::prelude::*;
 
