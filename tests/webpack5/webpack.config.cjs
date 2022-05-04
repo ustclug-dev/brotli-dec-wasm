b0b58f649ef94cf7f5234ea7045d2b36a9267cd8
myl7 <myl@myl.moe>
2022-05-04 12:20:52 +0800

    Update tests to include streaming and make it runnable

--- tests/webpack5/webpack.config.cjs
+++ tests/webpack5/webpack.config.cjs
@@ -1,12 +1,16 @@
 const path = require('path')
 
 module.exports = {
-    entry: './index.js',
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    experiments: {
-        asyncWebAssembly: true
-    }
+  entry: './index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  experiments: {
+    asyncWebAssembly: true,
+  },
+  optimization: {
+    minimize: false,
+  },
+  mode: 'development',
 }
