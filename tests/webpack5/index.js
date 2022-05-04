2cf9fdf9264c90f095c602e2162f022184ea658b
myl7 <myl@myl.moe>
2022-05-04 12:20:52 +0800

    Update tests to include streaming and make it runnable

--- tests/webpack5/index.js
+++ tests/webpack5/index.js
@@ -1,5 +1,21 @@
-import {brotliDec} from 'brotli-dec-wasm'
+const input = new Uint8Array([
+  27, 23, 0, 248, 141, 148, 169, 61, 94, 181, 142, 183, 224, 96, 98, 170, 98, 161, 150, 56, 160, 232, 44, 0,
+])
+const output = new Uint8Array([
+  227, 130, 173, 227, 131, 169, 227, 130, 173, 227, 131, 169, 227, 131, 137, 227, 130, 173, 227, 131, 137, 227, 130,
+  173,
+])
 
-let input = new Uint8Array([27, 23, 0, 248, 141, 148, 169, 61, 94, 181, 142, 183, 224, 96, 98, 170, 98, 161, 150, 56, 160, 232, 44, 0])
-let output = new Uint8Array([227, 130, 173, 227, 131, 169, 227, 130, 173, 227, 131, 169, 227, 131, 137, 227, 130, 173, 227, 131, 137, 227, 130, 173])
-console.assert(brotliDec(input) === output)
+import('brotli-dec-wasm').then(({ brotliDec, BrotliDecStream, BrotliDecStreamResultCode }) => {
+  const res1 = brotliDec(input)
+  console.log(res1)
+  console.assert(res1.length === output.length)
+  res1.forEach((v, i) => console.assert(v === output[i]))
+
+  const dec = new BrotliDecStream()
+  const { code, output: res2 } = dec.dec(input, 2048)
+  console.log(code)
+  console.log(res2)
+  console.assert(code === BrotliDecStreamResultCode.ResultSuccess)
+  res2.forEach((v, i) => console.assert(v === output[i]))
+})
