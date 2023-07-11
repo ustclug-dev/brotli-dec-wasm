fb093eaa0a55fb7b27dc02f0f0c38c7c8243b1a1
myl7 <myl@myl.moe>
2023-07-11 17:52:27 +0800

    Add module entry like brotli-wasm

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { BrotliDecStream } from './pkg/index.js'
+import * as brotliDecWasm from './pkg/index.js'
 
 export const DecompressStream = BrotliDecStream
 
 export * from './pkg/index.js'
+
+// The following bootstrapping code is to match `brotli-wasm`
+export default async () => brotliDecWasm
