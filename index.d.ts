a3d3b16b49b4d99ac3fdaa965b7a95f39671a6aa
myl7 <myl@myl.moe>
2023-11-20 19:39:38 +0800

    Export init and wasm binary file

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
 import type * as BrotliDecWasm from './pkg/index.d.ts'
+import type init from './pkg/index.d.ts'
 
 export type * from './pkg/index.d.ts'
+export { init }
 
 declare const BrotliDecWasmTypePromise: Promise<typeof BrotliDecWasm>
 export default BrotliDecWasmTypePromise
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import init, * as brotliDecWasm from './pkg/index.js'
 
 export * from './pkg/index.js'
+export { init }
 
 export default init().then(() => brotliDecWasm)
